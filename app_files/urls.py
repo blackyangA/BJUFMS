3281e34429e8fdc70b3530d1353fed86f9611b46
931395490@qq.com <931395490@qq.com>
2022-05-03 18:53:51 +0800

    update urls

--- app_files/urls.py
+++ app_files/urls.py
@@ -9,12 +9,21 @@ router.register(r'document', views.DocumentFileViewSet)
 router.register(r'account', views.AccountingFileViewSet)
 router.register(r'physical', views.PhysicalFileViewSet)
 router.register(r'electronic', views.ElectronicFileViewSet)
-router.register(r'approval', views.ElectronicFileViewSet)
-router.register(r'budget', views.ElectronicFileViewSet)
-router.register(r'audit', views.ElectronicFileViewSet)
-router.register(r'launch', views.ElectronicFileViewSet)
-router.register(r'acceptance', views.ElectronicFileViewSet)
-router.register(r'management', views.ElectronicFileViewSet)
+router.register(r'approval', views.ProjectApprovalInformationViewSet)
+router.register(r'budget', views.BudgetInformationViewSet)
+router.register(r'audit', views.AuditinformationViewSet)
+router.register(r'launch', views.LaunchInformationViewSet)
+router.register(r'acceptance', views.AcceptanceInformationViewSet)
+router.register(r'management', views.ProjectManagementViewSet)
+
+"""
+approval 立项信息
+budget 预算信息表
+audit 审计信息
+launch 启动信息
+acceptance 验收信息
+management 工程管理
+"""
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
